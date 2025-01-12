69bcb4351b7484f5be45d2aee7230f2bcbe1f5ed
xSpeed <bulacuandrei66@gmail.com>
2025-01-12 16:48:51 +0200

    Level

--- Galuste/Assets/levelgenerator.cs
+++ Galuste/Assets/levelgenerator.cs
@@ -5,8 +5,8 @@ public class levelgenerator : MonoBehaviour
     public GameObject platformus;
     public int numardeplatformus = 200;
     public float latimenivel = 3f;
-    public float minY = .2f;
-    public float maxY = 1.5f;
+    public float minY = .1f;
+    public float maxY = 1.3f;
 
     void Start()
     {
@@ -14,6 +14,7 @@ public class levelgenerator : MonoBehaviour
         for (int i = 0; i < numardeplatformus; i++)
         {
             spawnPosition.y += Random.Range(minY, maxY);
+            spawnPosition.x = Random.Range(-latimenivel, latimenivel);
             Instantiate(platformus, spawnPosition, platformus.transform.rotation);
         }
     }
