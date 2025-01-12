b6f9da219bf46254986b2445814ebdec48764be2
ScytheCleave <dvdoctav5@gmail.com>
2025-01-12 03:35:39 +0200

    generaredeplatformus1

---
+++ Galuste/Assets/levelgenerator.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class levelgenerator : MonoBehaviour
+{
+    public GameObject platformus;
+    public int numardeplatformus = 200;
+    public float latimenivel = 3f;
+    public float minY = .2f;
+    public float maxY = 1.5f;
+
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3();
+        for (int i = 0; i < numardeplatformus; i++)
+        {
+            spawnPosition.y = Random.Range(minY, maxY);
+            Instantiate(platformus, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
